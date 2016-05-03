178b7a35e883114a798187f4bf466620f3268a5a
Thomas Roughton <t.roughton@me.com>
2016-05-03 17:05:58 +1200

    Initial Commit

---
+++ XMLSchemaGeneration/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XMLSchemaGeneration
+//
+//  Created by Thomas Roughton on 3/05/16.
+//  Copyright © 2016 Thomas Roughton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
